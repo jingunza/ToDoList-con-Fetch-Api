b4c7f7ac3f484313ff1b11044e0a3ecbd79ec28d
Jose Ingunza <jose.ingunza.es@gmail.com>
2022-07-18 00:32:16 +0000

    avance version 18.07.22

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -13,92 +13,23 @@ const App = () => {
   const [registro, setRegistro] = useState([]);
   const [value, setValue] = useState('');
 
-  const [currentUser, setCurrentUser] = useState('');
-  const [inputUser, setInputUser] = useState('');
-
-  const [estiloInputTarea, setEstiloInputTarea] = useState('hidden');
+  // const [currentUser, setCurrentUser] = useState('');
+  // const [inputUser, setInputUser] = useState('');
 
   /* --------------- FUNCIONES Y METODOS DE FETCH API------------------------------- */
 
-  // useEffect(()=>{  //pide ingresar User
-  //   alert('ingresa tu usuario en el pie de pagina')
-  // },[]);
+  // useEffects----------------------------------------------
+  useEffect(()=>{
+    get;
+    console.log(registro);
+  }, []);
 
-  //     // insertar usuario nuevo---------------------
-  // const insertarUser = (e) => { //esta funcion se ejecuta en onKeyDown al dar Enter
-  //   if(e.target.value.trim().length!==0 && e.key==='Enter'){
-  //     setCurrentUser(e.target.value);
-  //     setInputUser(''); //el value de este elemento input lee a la variable inputUser
-  //     setEstiloInputTarea('visible');
-  //   }
-  // };
+  useEffect(()=>{
+    put;
+    console.log(registro);
+  }, [registro]);
 
-  // useEffect(()=>{  
-  //   fetch(`https://assets.breatheco.de/apis/fake/todos/user/${currentUser}`)
-  //     .then((response)=> {
-  //       if (!response.ok) {
-  //         throw Error(response.statusText);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((responseAsJson)=> {
-  //       setRegistro(responseAsJson);
-  //     })
-  //     .catch((error)=> {
-  //         console.log('Looks like there was a problem: \n', error);
-  //     });
-  // },[insertarUser]);
-
-  //     // get inicial de la aplicacion -------------
-  // const getInit = (apendice) =>{
-  //   fetch('https://assets.breatheco.de/apis/fake/todos/user/'+ apendice)
-  //     .then((response)=> {
-  //       if (!response.ok) {
-  //         post(apendice);  // mas abajo se define post: se ejecuta solo cuando falla el get 
-  //         throw Error(response.statusText);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((responseAsJson)=> {
-  //       setRegistro(responseAsJson);
-  //     })
-  //     .catch((error)=> {
-  //         console.log('Looks like there was a problem: \n', error);
-  //     });
-  // };
-
-  //     // post que se ejecuta si falla el get inicial ----------------
-  // const post = (apendice) =>{ //solo se ejecuta dentro del get si este falla
-  //   fetch('https://assets.breatheco.de/apis/fake/todos/user/'+ apendice, {
-  //     method: "POST",
-  //     body: JSON.stringify([{"label":"sample task", "done": false }]),
-  //     headers: {"Content-type": "application/json; charset=UTF-8"}
-  //   })
-  //   .then(response =>response.json()) 
-  //   .then(json => console.log(json))
-  //   .catch(err => console.log(err));
-  // };
-
-      // funcion insertar nueva tarea y accesorios -------------------
-  let currentTime = new Date();
-  let hora = currentTime.getHours().toString();
-  let minuto = currentTime.getMinutes().toString();
-  let segundo = currentTime.getSeconds().toString();
-  const insertarTarea = (e) =>{ //esto se ejecuta en el onKeyDown
-    if(e.target.value.trim().length!==0 && e.key==='Enter'){
-      setRegistro([...registro, {label: e.target.value, done: false}]); //esto SOLO funciona con DECONSTRUCCION, NO USAR PUSH
-      setValue('');
-      put(currentUser);
-      };
-  };
-
-      // funcion eliminar tarea y accesorios -------------------
-  const eliminarTarea = (item) => {
-    setRegistro(registro.filter((elem)=>elem!==item)); //referencio al id del elemento del array que voya a eliminar dentro del map
-    put(currentUser);
-  };
-
-      // get recurrente sin post incluido ------------------------------
+  // metodo get -------------------------------------------
   const get = () =>{ // este get se usara despues de cada put
     fetch('https://assets.breatheco.de/apis/fake/todos/user/joseingunza')
       .then((response)=> {
@@ -108,16 +39,17 @@ const App = () => {
         return response.json();
       })
       .then((responseAsJson)=> {
+        console.log(responseAsJson);
         setRegistro(responseAsJson);
       })
       .catch((error)=> {
           console.log('Looks like there was a problem: \n', error);
       });
   };
-
-      // put para actualizar informacion en la API ---------------------
-  const put = (apendice) => {
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/'+ apendice, {
+      
+  // put para actualizar informacion en la API ---------------------
+  const put = () => {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/joseingunza', {
       method: "PUT",
       body: JSON.stringify(registro),
       headers: {
@@ -134,11 +66,35 @@ const App = () => {
     });
   };
 
+      // funcion insertar nueva tarea y accesorios -------------------
+  let currentTime = new Date();
+  let hora = currentTime.getHours().toString();
+  let minuto = currentTime.getMinutes().toString();
+  let segundo = currentTime.getSeconds().toString();
+  const insertarTarea = (e) =>{ //esto se ejecuta en el onKeyDown
+    if(e.target.value.trim().length!==0 && e.key==='Enter'){
+      setRegistro([...registro, {label: e.target.value, done: false}]); //esto SOLO funciona con DECONSTRUCCION, NO USAR PUSH
+      setValue('');
+      // put();
+      };
+  };
+
+      // funcion eliminar tarea y accesorios -------------------
+  const eliminarTarea = (item) => {
+    setRegistro(registro.filter((elem)=>elem!==item)); //referencio al id del elemento del array que voya a eliminar dentro del map
+    // put();
+  };
+
+  const eliminarLista = () =>{
+    setRegistro([]);
+    // put();
+  } 
+
   /* --------------------------RENDERIZACION------------------------------------------ */
 
 	return (
 		<div className="body-hijo row justify-content-center">
-			<div className="cuaderno col-10 col-sm-8 col-md-6 col-lg-5 pt-5 pb-3 px-0 mt-5">
+			<div className="cuaderno col-10 col-sm-8 col-md-6 col-lg-5 pt-5 pb-2 px-0 mt-5">
 				<h1 className="titulo-lista text-center mt-0 mb-4">todos</h1>
         <div className="zona-rayada">
           
@@ -151,7 +107,6 @@ const App = () => {
             className="entrada-tarea px-3 px-lg-5" 
             type="text"
             placeholder="What needs to be done?"
-            style={{visibility: estiloInputTarea}} 
             />
           </div>
 
@@ -176,20 +131,11 @@ const App = () => {
               </div>)}
           </div>  
 
-					<div className="pie-pagina d-flex justify-content-between pt-4 pb-2 px-3">
-              <div className="col-3">{registro.length} items left</div>
-              <div className="contenedor-user col-6">
-                <input
-                  onKeyDown={insertarUser}
-                  value ={inputUser} //input controlado de User
-                  onChange={(e)=>{setInputUser(e.target.value)}} //input controlado de User
-
-                  className="entrada-user px-3 px-lg-5"
-                  placeholder="ingrese usuario"
-                  type="text"
-                />
+					<div className="pie-pagina d-flex justify-content-between pt-2 pb-1 px-3">
+              <div className="col-6">{registro.length} items left</div>
+              <div className="contenedor-delete col-6 text-end">
+                <button className="btn btn-danger" onClick={eliminarLista}>Eliminar Lista</button>
               </div>
-              <div className="col-3 text-end">{(currentUser!=='')? 'user: '+currentUser : ''}</div>
           </div>
 
         </div>
@@ -197,7 +143,5 @@ const App = () => {
 		</div>	
 	);
 };
-
-/* -----------------------EXPORTACION DE COMPONENTE----------------------------------- */
 
 export default App;
